a9de3eb58a72965b7ef9774ecb85ad2546cbf51a
scottzyang <scottzyang@gmail.com>
2022-11-30 20:40:45 -0600

    Add two additional tests

--- virus.py
+++ virus.py
@@ -16,3 +16,14 @@ if __name__ == "__main__":
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+# two additional tests
+virus2 = Virus("COVID", 1.0, 0.25)
+assert virus2.name == "COVID"
+assert virus2.repro_rate == 1.0
+assert virus2.mortality_rate == 0.25
+
+virus3 = Virus("H1N1", 0.2, 0.4)
+assert virus3.name == "H1N1"
+assert virus3.repro_rate == 0.2
+assert virus3.mortality_rate == 0.4
