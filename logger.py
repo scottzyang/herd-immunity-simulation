578d57b3b33c2f874e96234a130261109c99cfba
scottzyang <scottzyang@gmail.com>
2022-12-09 11:00:51 -0600

    Modify final_data and meta_data methods

--- logger.py
+++ logger.py
@@ -29,7 +29,7 @@ class Logger(object):
         # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        filename = open(self.file_name, 'a')
+        filename = open(self.file_name, 'w')
         filename.write(f'Population size: {pop_size}\nVaccinated Percentage: {vacc_percentage}\nVirus Name: {virus_name}\nMortality Rate: {mortality_rate}\nReproduction Rate: {basic_repro_num}\n\n')
         filename.close()
 
@@ -55,7 +55,7 @@ class Logger(object):
         filename.write(f'Step Number:{time_step_number}\n')
         filename.close()
 
-    def final_data(self, survivors, fatalities, vaccinated_pop, interactions, infections, vax_saves):
+    def final_data(self, survivors, fatalities, final_infections, vaccinated_pop, interactions, infections, vax_saves):
         filename = open(self.file_name, 'a')
-        filename.write(f'Survivors: {survivors}\nFatalities: {fatalities}\nVaccinated Populations: {vaccinated_pop}\nTotal Interactions: {interactions}\nTotal Infections: {infections}\nSaved by Vaccines: {vax_saves}')
+        filename.write(f'Survivors: {survivors}\nFatalities: {fatalities}\nFinal population Infected: {final_infections}\nVaccinated Populations: {vaccinated_pop}\nTotal Interactions: {interactions}\nTotal Infections: {infections}\nSaved by Vaccines: {vax_saves}')
         filename.close()
